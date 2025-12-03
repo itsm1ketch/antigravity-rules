476171c0f0d6ed085477e835873e6ca470f6b591
Michael Tcherniack <itsm1ketch@gmail.com>
2025-12-03 19:26:13 +0000

    Initial commit

---
+++ src/rules/example-rule.ts
@@ -0,0 +1,19 @@
+export const exampleRule = [
+  {
+    tags: ["Example", "Open Source"],
+    title: "Example Antigravity Rule",
+    slug: "example-antigravity-rule",
+    libs: [],
+    content: `
+    # Example Rule
+    
+    This is a rule defined in the open-source package.
+    `,
+    author: {
+      name: "Open Source Contributor",
+      url: "https://github.com",
+      avatar: "",
+    },
+  },
+];
+
